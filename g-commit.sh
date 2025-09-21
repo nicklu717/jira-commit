fd9daea8af892a064eaaeda1cd4fa39cf1e234b4
Nick Lu <nicklu717@gmail.com>
2025-09-21 08:18:26 +0800

    feat: grep issue key

--- g-commit.sh
+++ g-commit.sh
@@ -2,8 +2,11 @@
 
 # set -x
 
+current_branch=$(git branch --show-current)
+issue_key=$(echo "${current_branch}" | grep -Eo "PROJ-\d+")
+
 type=$(gum choose "feat" "fix" "chore" "refactor" "test" "docs")
-summary=$(gum input --value "${type}: " --placeholder "Summary of this commit")
+summary=$(gum input --value "[${issue_key}] ${type}: " --placeholder "Summary of this commit")
 description=$(gum write --placeholder "(optional) Description of this commit")
 echo "[SUMMARY] ${summary}"
 echo "[DESCRIPTION] ${description}"
