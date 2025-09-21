90d4d3cd1392bb5941600389c64b4bd07eddd8c2
Nick Lu <nicklu717@gmail.com>
2025-09-21 13:30:52 +0800

    fix: hide issue key prefix if not found

--- g-commit.sh
+++ g-commit.sh
@@ -4,9 +4,12 @@
 
 current_branch=$(git branch --show-current)
 issue_key=$(echo "${current_branch}" | grep -Eo "PROJ-\d+")
+if [[ -n "${issue_key}" ]]; then
+    issue_key_prefix="[${issue_key}] "
+fi
 
 type=$(gum choose "feat" "fix" "chore" "refactor" "test" "docs")
-summary=$(gum input --value "[${issue_key}] ${type}: " --placeholder "Summary of this commit")
+summary=$(gum input --value "${issue_key_prefix}${type}: " --placeholder "Summary of this commit")
 description=$(gum write --placeholder "(optional) Description of this commit")
 
-git commit -m "${summary//\"/\\"}" -m "${description}"
+git commit -m "${summary}" -m "${description}"
