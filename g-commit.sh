05adab05375c434455b5c40582d4587f91b127d9
Nick Lu <nicklu717@gmail.com>
2025-09-21 13:14:12 +0800

    feat: add commit command

--- g-commit.sh
+++ g-commit.sh
@@ -8,5 +8,5 @@ issue_key=$(echo "${current_branch}" | grep -Eo "PROJ-\d+")
 type=$(gum choose "feat" "fix" "chore" "refactor" "test" "docs")
 summary=$(gum input --value "[${issue_key}] ${type}: " --placeholder "Summary of this commit")
 description=$(gum write --placeholder "(optional) Description of this commit")
-echo "[SUMMARY] ${summary}"
-echo "[DESCRIPTION] ${description}"
+
+git commit -m "${summary//\"/\\"}" -m "${description}"
